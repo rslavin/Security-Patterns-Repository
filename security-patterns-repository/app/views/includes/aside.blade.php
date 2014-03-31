0986dfb9d81fe4920bd7510953e8194a3b047c4c
Jean-Michel Lehker <j.lehker@gmail.com>
2014-03-31 16:19:58 -0500

    changed login form to search form

--- security-patterns-repository/app/views/includes/aside.blade.php
+++ security-patterns-repository/app/views/includes/aside.blade.php
@@ -1,4 +1,4 @@
-<section class="loginform cf">
+<section class="searchform cf">
 <form name="search" action="/repository/patterns/search" method="post" accept-charset="utf-8">
 	<ul>
 		<li><input type="search" name="q" required></li>
