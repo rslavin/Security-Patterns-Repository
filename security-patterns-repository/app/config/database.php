8def85244c1eb96644fbec36e6b859a27e77d1fd
Jean-Michel Lehker <j.lehker@gmail.com>
2014-04-25 16:06:14 -0500

    database config

--- security-patterns-repository/app/config/database.php
+++ security-patterns-repository/app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'repository',
+			'username'  => 'papasmurf',
+			'password'  => 'vRU.64jYwJ',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
