7fcf8a20cebb4fc7ed71269c711e38e4d23e285b
Rocky Slavin <rocky.slavin@gmail.com>
2014-04-25 15:08:01 -0500

    /types describes pattern types; added navigation menu to include a link to /types

---
+++ security-patterns-repository/app/views/pages/types.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.default')
+@section('content')
+	<header>
+		<h1>Pattern Types</h1>
+		<hr/>
+	</header>
+	
+	<section>
+    	<h2><a href="/patterns/design">Design</a></h2>
+		<p>The design pattern is the most abundant type of pattern. Such patterns 
+			Describe a solution to a recurring problem for software design.
+			Most other pattern types are based on the idea of a design pattern.</p>
+	</section>
+	<br />
+	
+	<section>
+    	<h2><a href="/patterns/design">Requirement</a></h2>
+		<p>Requirements patterns provide a solution to recurring requirements problems
+			or situations. These types of patterns do not deal with the design or 
+			implementation of a system. Instead, they provide clear orgnaization
+			for eliciting and creating requirements for the system.</p>
+	</section>
+	<br />
+	
+	<section>
+    	<h2><a href="/patterns/design">Architectural</a></h2>
+		<p>TODO</p>
+	</section>
+	<br />
+	
+	<section>
+    	<h2><a href="/patterns/design">Implementation</a></h2>
+		<p>TODO</p>
+	</section>
+	<br />
+	
+	<section>
+    	<h2><a href="/patterns/design">Procedural</a></h2>
+		<p>TODO</p>
+	</section>
+	<br />
+	
+	<section>
+    	<h2><a href="/patterns/design">Mini</a></h2>
+		<p>We classify solutions for recurring problems which are not specified in 
+			large amounts of text as mini patterns. Generally, such patterns address
+			a simple problem with a solution that can be fully described on a single 
+			note card. In some cases, these patterns are explicitely described as mini
+			patterns by their respective authors and may have larger representations.</p>
+	</section>
+	<br />
+@stop
