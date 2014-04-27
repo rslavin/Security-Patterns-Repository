f780d4f3492107907425987d25d805e5285dc4b8
Rocky Slavin <rocky.slavin@gmail.com>
2014-04-27 13:40:22 -0500

    Added pattern type descriptions

--- security-patterns-repository/app/views/pages/types.blade.php
+++ security-patterns-repository/app/views/pages/types.blade.php
@@ -7,9 +7,14 @@
 	
 	<section>
     	<h2><a href="/patterns/design">Design</a></h2>
+    	<strong>Description</strong>
 		<p>The design pattern is the most abundant type of pattern. Such patterns 
 			Describe a solution to a recurring problem for software design.
 			Most other pattern types are based on the idea of a design pattern.</p>
+		
+		<strong>Criteria</strong>
+		<p>Patterns were categorized as <i>design</i> patterns if their respective
+			authors described them as such.</p>
 	</section>
 	<br />
 	
@@ -17,26 +22,49 @@
     	<h2><a href="/patterns/design">Requirement</a></h2>
 		<p>Requirements patterns provide a solution to recurring requirements problems
 			or situations. These types of patterns do not deal with the design or 
-			implementation of a system. Instead, they provide clear orgnaization
+			implementation of a system. Instead, they provide clear organization
 			for eliciting and creating requirements for the system.</p>
+			
+			<strong>Criteria</strong>
+		<p>Patterns were categorized as <i>requirements</i> patterns if their respective
+			authors described them as such.</p>
 	</section>
 	<br />
 	
 	<section>
     	<h2><a href="/patterns/design">Architectural</a></h2>
-		<p>TODO</p>
+		<p>Architectural patterns describe a recurring solution to an architectural
+			design problem. Such patterns generally include design, requirements, security, 
+			and even hardware information.</p>
+			
+			<strong>Criteria</strong>
+		<p>Patterns were categorized as <i>architectural</i> patterns if their respective
+			authors described them as such.</p>
 	</section>
 	<br />
 	
 	<section>
     	<h2><a href="/patterns/design">Implementation</a></h2>
-		<p>TODO</p>
+		<p>Implementation patterns focus on helping software developers write effective 
+			and flexible code. </p>
+			
+			<strong>Criteria</strong>
+		<p>Patterns were categorized as <i>implementation</i> patterns if their respective
+			authors described them as such.</p>
 	</section>
 	<br />
 	
 	<section>
     	<h2><a href="/patterns/design">Procedural</a></h2>
-		<p>TODO</p>
+		<p>Procedural patterns are similar to design patterns in that they describe a way 
+			to design a solution to a recurring problem. The difference is that procedural
+			patterns generally provide a method or set of steps to accomplish something. Such
+			patterns will describe the actions that need to be taken by the system and the
+			users of the system in order to accomplish the procedural task.</p>
+			
+			<strong>Criteria</strong>
+		<p>Patterns were categorized as <i>procedural</i> patterns if their respective
+			authors described them as such.</p>
 	</section>
 	<br />
 	
